4c87c03e8b28234380c2c0b12cd0e6e6c59e441c
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-02-25 21:46:15 +0000

    WIP: GRPC server

---
+++ app/grpc/server.py
@@ -0,0 +1,33 @@
+from concurrent import futures
+import time
+
+import grpc
+
+import prediction_pb2_grpc
+import prediction_pb2
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class PredictionServiceServicer(prediction_pb2_grpc.PredictionServiceServicer):
+    def GetForFixture(self, request, context):
+        pred = prediction_pb2.Prediction(id=request.id, type='Over 2.5 goals', probability=0.75)
+        return pred
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    prediction_pb2_grpc.add_PredictionServiceServicer_to_server(
+        PredictionServiceServicer(), server
+    )
+    print('Starting server. Listing on port 50051')
+    server.add_insecure_port('grpc:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+serve()
