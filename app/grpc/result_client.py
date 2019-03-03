f8efa127146e048c624dabf48ef925a11c13f989
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-03-03 19:24:01 +0000

    Yield results in result client

--- app/grpc/result_client.py
+++ app/grpc/result_client.py
@@ -8,7 +8,8 @@ class ResultClient:
     def GetResultsForTeam(self, team_id, limit, date_before):
         client = self.client()
         request = result_pb2.TeamRequest(team_id=team_id, limit=limit, date_before=date_before)
-        return client.GetResultsForTeam(request)
+        for result in client.GetResultsForTeam(request):
+            yield result
 
     def client(self):
         data_server = os.getenv('DATA_SERVER_HOST')
