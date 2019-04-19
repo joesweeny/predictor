b56b0f7f8f509f77b3d6b6b4c7ce13c8ef1901dd
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-04-19 19:34:18 +0100

    Implement new Result proto methods in Result Client

--- app/grpc/client/result_client.py
+++ app/grpc/client/result_client.py
@@ -6,14 +6,32 @@ from google.protobuf import wrappers_pb2
 
 
 class ResultClient:
+    def GetResultsForSeason(self, season_id):
+        client = self.__client()
+        request = result_pb2.SeasonRequest(season_id=season_id)
+        for result in client.GetResultsForSeason(request):
+            yield result
+
     def GetResultsForTeam(self, team_id, limit, date_before):
-        client = self.client()
+        client = self.__client()
         limit = wrappers_pb2.Int32Value(value=limit)
         request = result_pb2.TeamRequest(team_id=team_id, limit=limit, date_before=date_before)
         for result in client.GetResultsForTeam(request):
             yield result
 
-    def client(self):
+    def GetHistoricalResultsForFixture(self, home_team_id, away_team_id, limit, date_before):
+        client = self.__client()
+        request = result_pb2.HistoricalResultRequest(
+            home_team_id=home_team_id,
+            away_team_id=away_team_id,
+            limit=limit,
+            date_before=date_before
+        )
+        for result in client.GetHistoricalResultsForFixture(request):
+            yield result
+
+    @staticmethod
+    def __client():
         data_server = os.getenv('DATA_SERVER_HOST')
         channel = grpc.insecure_channel(data_server + ':50051')
         stub = result_pb2_grpc.ResultServiceStub(channel)
