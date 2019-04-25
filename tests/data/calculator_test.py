f970eaa3e2df0953f9fa8aab8897e2f36b6ea7eb
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-04-25 10:42:38 +0100

    Remove use of google wrappers in tests

--- tests/data/calculator_test.py
+++ tests/data/calculator_test.py
@@ -1,12 +1,11 @@
 from predictor.grpc.proto.result.result_pb2 import MatchStats
 from predictor.data import calculator
-from google.protobuf import wrappers_pb2
 
 
 def test_total_goals_returns_home_and_away_goals_combined():
     stats = MatchStats()
-    stats.home_score.value = wrappers_pb2.Int32Value(value=5).value
-    stats.away_score.value = wrappers_pb2.Int32Value(value=0).value
+    stats.home_score.value = 5
+    stats.away_score.value = 0
 
     assert calculator.TotalGoalsForMatch(stats) == 5
 
@@ -19,14 +18,14 @@ def test_total_goals_returns_none_if_both_required_values_are_not_present():
 
 def test_total_goals_returns_none_if_either_required_value_is_missing():
     stats = MatchStats()
-    stats.home_score.value = wrappers_pb2.Int32Value(value=5).value
+    stats.home_score.value = 5
 
     assert calculator.TotalGoalsForMatch(stats) is None
 
 
 def test_total_goals_can_handle_zero_values():
     stats = MatchStats()
-    stats.home_score.value = wrappers_pb2.Int32Value(value=0).value
-    stats.away_score.value = wrappers_pb2.Int32Value(value=0).value
+    stats.home_score.value = 0
+    stats.away_score.value = 0
 
     assert calculator.TotalGoalsForMatch(stats) == 0
