18bbae8efdb57322eb83f832964767a46363f38e
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-05-07 12:43:30 +0100

    Add team stats grpc client

---
+++ predictor/grpc/team_stats_client.py
@@ -0,0 +1,19 @@
+import grpc
+from predictor.grpc.proto.stats.team import stats_pb2
+from predictor.grpc.proto.stats.team import stats_pb2_grpc
+
+
+class TeamStatsClient:
+    def __init__(self, host: str, port: str):
+        self.host = host
+        self.port = port
+
+    def get_team_stats_for_fixture(self, fixture_id: int):
+        client = self.__client()
+        request = stats_pb2.FixtureRequest(fixture_id=fixture_id)
+        return client.GetTeamStatsForFixture(request)
+
+    def __client(self):
+        channel = grpc.insecure_channel(self.host + ':' + self.port)
+        stub = stats_pb2_grpc.TeamStatsServiceStub(channel)
+        return stub
