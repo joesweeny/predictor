7a20ece7b16a85d3d1698645381e88b17308843b
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-06-04 20:16:55 +0100

    Remove unused data handler method

--- predictor/data/handling/data_handler.py
+++ predictor/data/handling/data_handler.py
@@ -39,32 +39,3 @@ class DataHandler:
                            + str(season['id'])
 
                 self._repository.save_data_frame(key=filename, df=df)
-
-    def store_match_goals_data_for_specific_competition(
-        self,
-        competition_id: int,
-        date_before: datetime
-    ):
-        """
-        For a given competition, if supported
-            a) parse data into dataframe
-            b) store dataframe to persistence layer
-        """
-        competitions = self._configuration.SUPPORTED_COMPETITIONS
-
-        for i, competition in competitions.items():
-            if competition['id'] == competition_id:
-                seasons = competition['seasons']
-
-                for season in seasons:
-                    df = self._aggregator.for_season(
-                        season_id=season['id'],
-                        date_before=date_before
-                    )
-
-                    filename = "competition:" \
-                               + str(competition_id) \
-                               + ':season:' \
-                               + str(season['id'])
-
-                    self._repository.save_data_frame(key=filename, df=df)
