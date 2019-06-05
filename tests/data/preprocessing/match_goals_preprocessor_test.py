896d3fff43c17a76b87a02102db445e3532e3ec9
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-06-05 21:48:12 +0100

    Add pre processor method test

--- tests/data/preprocessing/match_goals_preprocessor_test.py
+++ tests/data/preprocessing/match_goals_preprocessor_test.py
@@ -1,13 +1,15 @@
 import pytest
+import mock
 from typing import Optional
 from freezegun import freeze_time
-from datetime import datetime
+from datetime import datetime, timezone
 from mock import Mock, call
 import pandas as pd
 from predictor.data.preprocessing.match_goals import MatchGoalsPreProcessor
 from predictor.data.aggregator.match_goals import MatchGoals
 from predictor.data.repository.redis import RedisRepository
 from predictor.framework import config
+from predictor.framework.exception import DataPreProcessingException
 
 
 @freeze_time('2019-06-05')
@@ -23,28 +25,43 @@ def test_pre_process_feature_data_for_competition_aggregates_historic_data_and_s
         configuration=configuration
     )
 
-    preprocessor.pre_process_feature_data_for_competition(competition_id=8)
-
     df1 = get_season_data_frame(date='2019-05-31', stat=1.5)
     df2 = get_season_data_frame(date='2019-05-24', stat=2.5)
-    df3 = get_season_data_frame(date='2019-06-05', stat=3.75)
 
-    aggregator.for_season.side_effect = [df1, df2, df3]
+    aggregator.for_season.side_effect = [df1, df2]
 
-    combined = pd.concat(df2, df1, df3)
+    preprocessor.pre_process_feature_data_for_competition(competition_id=8)
 
     aggregator_calls = [
-        call(season=13, date_before=datetime(2019, 6, 5)),
-        call(season=6397, date_before=datetime(2019, 6, 5)),
+        call(season_id=13, date_before=datetime(2019, 6, 5, 0, 0, tzinfo=timezone.utc)),
+        call(season_id=6397, date_before=datetime(2019, 6, 5, 0, 0, tzinfo=timezone.utc))
     ]
 
-    aggregator.assert_has_calls(aggregator_calls)
+    aggregator.for_season.assert_has_calls(aggregator_calls)
 
     cache_calls = [
-        call(key='match-goals:competition:8', df=combined)
+        call(key='match-goals:competition:8', df=mock.ANY)
     ]
 
-    cache.assert_has_calls(cache_calls)
+    cache.save_data_frame.assert_has_calls(cache_calls)
+
+
+def test_pre_process_feature_data_raises_exception_if_competition_is_not_supported(
+    preprocessor,
+    aggregator,
+    cache,
+    configuration
+):
+    preprocessor = MatchGoalsPreProcessor(
+        aggregator=aggregator,
+        cache=cache,
+        configuration=configuration
+    )
+
+    aggregator.for_season.side_effect = []
+
+    with pytest.raises(DataPreProcessingException):
+        preprocessor.pre_process_feature_data_for_competition(competition_id=999)
 
 
 @pytest.fixture
