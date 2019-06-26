21da1dce223e07f54527ca50c4df9f01def8b091
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-06-26 17:29:07 +0100

    Refactor match goals aggregator and tests

--- compiler/data/aggregator/match_goals.py
+++ compiler/data/aggregator/match_goals.py
@@ -5,7 +5,6 @@ from compiler.grpc.result_client import ResultClient
 from compiler.grpc.proto.result.result_pb2 import Result
 from compiler.grpc.team_stats_client import TeamStatsClient
 from compiler.grpc.proto.stats.team.stats_pb2 import TeamStats
-from compiler.data import calculator
 
 
 class MatchGoals:
@@ -20,12 +19,8 @@ class MatchGoals:
         'season',
         'homeTeam',
         'homeGoals',
-        'homeAvgScored',
-        'homeAvgConceded',
         'awayTeam',
         'awayGoals',
-        'awayAvgScored',
-        'awayAvgConceded',
     ]
 
     def for_season(self, season_id: int, date_before: datetime) -> pd.DataFrame:
@@ -48,74 +43,20 @@ class MatchGoals:
 
         return df
 
-    def __fixture_to_row(self, fixture: Fixture):
+    @staticmethod
+    def __fixture_to_row(fixture: Fixture) -> dict:
         home_team = fixture.home_team
         away_team = fixture.away_team
 
         date = pd.to_datetime(datetime.utcfromtimestamp(fixture.date_time), format='%Y-%m-%dT%H:%M:%S')
-
-        home_previous_results = self.__get_previous_results(date, home_team.id, 5)
-        away_previous_results = self.__get_previous_results(date, away_team.id, 5)
-
-        historical_results = self.__get_historical_results(
-            date_before=date,
-            home_team_id=home_team.id,
-            away_team_id=away_team.id,
-            limit=10
-        )
 
         data = {
             'matchID': fixture.id,
             'round': fixture.round.name,
             'date': date,
             'season': fixture.season.name,
-            'averageGoalsForFixture': calculator.AverageGoalsForResults(
-                historical_results
-            ),
-            'homeTeamID': home_team.id,
             'homeTeam': home_team.name,
-            'homeDaysSinceLastMatch': calculator.days_between_results(
-                date,
-                home_previous_results[0],
-            ),
-            'homeAvgScoredLast5': calculator.AverageGoalsScoredByTeam(
-                home_previous_results,
-                home_team.id
-            ),
-            'homeAvgConcededLast5': calculator.AverageGoalsConcededByTeam(
-                home_previous_results,
-                home_team.id
-            ),
-            'homeScoredLastMatch': calculator.GoalsScoredInMatch(
-                home_previous_results[0],
-                home_team.id
-            ),
-            'homeConcededLastMatch': calculator.GoalsConcededInMatch(
-                home_previous_results[0],
-                home_team.id
-            ),
-            'awayTeamID': away_team.id,
             'awayTeam': away_team.name,
-            'awayDaysSinceLastMatch': calculator.days_between_results(
-                date,
-                away_previous_results[0]
-            ),
-            'awayAvgScoredLast5': calculator.AverageGoalsScoredByTeam(
-                away_previous_results,
-                away_team.id
-            ),
-            'awayAvgConcededLast5': calculator.AverageGoalsConcededByTeam(
-                away_previous_results,
-                away_team.id
-            ),
-            'awayScoredLastMatch': calculator.GoalsScoredInMatch(
-                away_previous_results[0],
-                away_team.id
-            ),
-            'awayConcededLastMatch': calculator.GoalsConcededInMatch(
-                away_previous_results[0],
-                away_team.id
-            ),
         }
 
         return data
@@ -128,9 +69,6 @@ class MatchGoals:
 
         date = pd.to_datetime(datetime.utcfromtimestamp(result.date_time), format='%Y-%m-%dT%H:%M:%S')
 
-        home_previous_results = self.__get_previous_results(date, home_team.id, 40)
-        away_previous_results = self.__get_previous_results(date, away_team.id, 40)
-
         data = {
             'matchID': result.id,
             'round': result.round.name,
@@ -138,40 +76,11 @@ class MatchGoals:
             'season': result.season.name,
             'homeTeam': home_team.name,
             'homeGoals': self.__get_value('home_score', match_stats),
-            'homeAvgScored': calculator.average_goals_scored_by_home_team(home_previous_results, home_team.id),
-            'homeAvgConceded': calculator.average_goals_conceded_by_home_team(home_previous_results, home_team.id),
             'awayTeam': away_team.name,
             'awayGoals': self.__get_value('away_score', match_stats),
-            'awayAvgScored': calculator.average_goals_scored_by_away_team(away_previous_results, away_team.id),
-            'awayAvgConceded': calculator.average_goals_conceded_by_away_team(away_previous_results, away_team.id),
         }
 
         return data
-
-    def __get_historical_results(
-        self,
-        date_before: datetime,
-        home_team_id: int,
-        away_team_id: int,
-        limit: int
-    ):
-        results = self.result_client.get_historical_results_for_fixture(
-            home_team_id=home_team_id,
-            away_team_id=away_team_id,
-            date_before=date_before.strftime('%Y-%m-%dT%H:%M:%S+00:00'),
-            limit=limit
-        )
-
-        return results[0:limit]
-
-    def __get_previous_results(self, date_before: datetime, team_id: int, limit: int):
-        results = self.result_client.get_results_for_team(
-            team_id=team_id,
-            limit=limit,
-            date_before=date_before.strftime('%Y-%m-%dT%H:%M:%S+00:00')
-        )
-
-        return results[0:limit]
 
     @staticmethod
     def __get_value(prop: str, stats: TeamStats):
--- tests/data/aggregator/match_goals_test.py
+++ tests/data/aggregator/match_goals_test.py
@@ -26,38 +26,9 @@ def test_for_season_data_frame_columns(match_goals):
         'round',
         'date',
         'season',
-        'averageGoalsForFixture',
-        'homeTeamID',
         'homeTeam',
-        'homeDaysSinceLastMatch',
-        'homeFormation',
-        'homeAvgScoredLast5',
-        'homeAvgConcededLast5',
-        'homeScoredLastMatch',
-        'homeConcededLastMatch',
-        'homeShotsTotal',
-        'homeShotsOnGoal',
-        'homeShotsOffGoal',
-        'homeShotsInsideBox',
-        'homeShotsOutsideBox',
-        'homeAttacksTotal',
-        'homeAttacksDangerous',
         'homeGoals',
-        'awayTeamID',
         'awayTeam',
-        'awayDaysSinceLastMatch',
-        'awayFormation',
-        'awayAvgScoredLast5',
-        'awayAvgConcededLast5',
-        'awayScoredLastMatch',
-        'awayConcededLastMatch',
-        'awayShotsTotal',
-        'awayShotsOnGoal',
-        'awayShotsOffGoal',
-        'awayShotsInsideBox',
-        'awayShotsOutsideBox',
-        'awayAttacksTotal',
-        'awayAttacksDangerous',
         'awayGoals',
     ]
 
@@ -66,38 +37,9 @@ def test_for_season_data_frame_columns(match_goals):
     assert (df_columns == columns).all()
 
 
-def test_for_season_converts_result_object_into_data_frame_row(
-    match_goals,
-    result,
-    home_past_result,
-    away_past_result,
-    team_stats_response
-):
+def test_for_season_converts_result_object_into_data_frame_row(match_goals, result):
     value = match_goals.result_client.get_results_for_season.return_value
     value.__iter__.return_value = iter([result])
-
-    match_goals.result_client.get_results_for_team.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ],
-        [
-            away_past_result,
-            away_past_result,
-            home_past_result,
-        ]
-    ]
-
-    match_goals.result_client.get_historical_results_for_fixture.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ]
-    ]
-
-    match_goals.team_stats_client.get_team_stats_for_fixture.return_value = team_stats_response
 
     df = match_goals.for_season(5, datetime.fromisoformat('2019-04-23T18:15:38+00:00'))
 
@@ -106,45 +48,14 @@ def test_for_season_converts_result_object_into_data_frame_row(
         date_before='2019-04-23T18:15:38+00:00'
     )
 
-    match_goals.team_stats_client.get_team_stats_for_fixture.assert_called_with(fixture_id=66)
-
     expected = [
         66,
         '4',
         pd.Timestamp('2019-04-23 18:15:38'),
         '2018/19',
-        6.00,
-        7901,
         'West Ham United',
-        3,
-        '4-4-2',
-        4.33,
-        1.67,
-        5,
         2,
-        34,
-        12,
-        22,
-        15,
-        5,
-        None,
-        None,
-        2,
-        496,
         'Tottenham Hotspur',
-        5,
-        '5-3-1-1',
-        1.33,
-        3.67,
-        1,
-        3,
-        34,
-        12,
-        22,
-        15,
-        5,
-        None,
-        None,
         2,
     ]
 
@@ -153,46 +64,9 @@ def test_for_season_converts_result_object_into_data_frame_row(
     assert row == expected
 
 
-def test_for_reason_populates_multiple_rows_of_data_for_multiple_results(
-        match_goals,
-        result,
-        home_past_result,
-        away_past_result
-):
+def test_for_reason_populates_multiple_rows_of_data_for_multiple_results(match_goals, result):
     value = match_goals.result_client.get_results_for_season.return_value
     value.__iter__.return_value = iter([result, result, result])
-
-    match_goals.result_client.get_results_for_team.side_effect = [
-        [home_past_result],
-        [away_past_result],
-        [home_past_result],
-        [away_past_result],
-        [home_past_result],
-        [away_past_result],
-        [home_past_result],
-        [away_past_result],
-        [home_past_result],
-        [away_past_result],
-        [home_past_result],
-    ]
-
-    match_goals.result_client.get_historical_results_for_fixture.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ],
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ],
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ]
-    ]
 
     df = match_goals.for_season(5, datetime.fromisoformat('2019-04-23T18:15:38+00:00'))
 
@@ -201,36 +75,10 @@ def test_for_reason_populates_multiple_rows_of_data_for_multiple_results(
         date_before='2019-04-23T18:15:38+00:00'
     )
 
-    assert df.shape == (3, 37)
+    assert df.shape == (3, 8)
 
 
-def test_for_fixture_data_frame_columns(
-    match_goals,
-    fixture,
-    home_past_result,
-    away_past_result
-):
-    match_goals.result_client.get_results_for_team.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ],
-        [
-            away_past_result,
-            away_past_result,
-            home_past_result,
-        ]
-    ]
-
-    match_goals.result_client.get_historical_results_for_fixture.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ]
-    ]
-
+def test_for_fixture_data_frame_columns(match_goals, fixture):
     df = match_goals.for_fixture(fixture=fixture)
 
     columns = [
@@ -238,74 +86,19 @@ def test_for_fixture_data_frame_columns(
         'round',
         'date',
         'season',
-        'averageGoalsForFixture',
-        'homeTeamID',
         'homeTeam',
-        'homeDaysSinceLastMatch',
-        'homeFormation',
-        'homeAvgScoredLast5',
-        'homeAvgConcededLast5',
-        'homeScoredLastMatch',
-        'homeConcededLastMatch',
-        'homeShotsTotal',
-        'homeShotsOnGoal',
-        'homeShotsOffGoal',
-        'homeShotsInsideBox',
-        'homeShotsOutsideBox',
-        'homeAttacksTotal',
-        'homeAttacksDangerous',
         'homeGoals',
-        'awayTeamID',
         'awayTeam',
-        'awayDaysSinceLastMatch',
-        'awayFormation',
-        'awayAvgScoredLast5',
-        'awayAvgConcededLast5',
-        'awayScoredLastMatch',
-        'awayConcededLastMatch',
-        'awayShotsTotal',
-        'awayShotsOnGoal',
-        'awayShotsOffGoal',
-        'awayShotsInsideBox',
-        'awayShotsOutsideBox',
-        'awayAttacksTotal',
-        'awayAttacksDangerous',
         'awayGoals',
     ]
 
     df_columns = df.columns
 
     assert (df_columns == columns).all()
-    assert df.shape == (1, 37)
+    assert df.shape == (1, 8)
 
 
-def test_for_fixture_returns_dataframe_of_collated_fixture_data(
-    match_goals,
-    fixture,
-    home_past_result,
-    away_past_result
-):
-    match_goals.result_client.get_results_for_team.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ],
-        [
-            away_past_result,
-            away_past_result,
-            home_past_result,
-        ]
-    ]
-
-    match_goals.result_client.get_historical_results_for_fixture.side_effect = [
-        [
-            home_past_result,
-            home_past_result,
-            away_past_result,
-        ]
-    ]
-
+def test_for_fixture_returns_dataframe_of_collated_fixture_data(match_goals, fixture):
     df = match_goals.for_fixture(fixture=fixture)
 
     row = df.iloc[0, :]
@@ -314,21 +107,8 @@ def test_for_fixture_returns_dataframe_of_collated_fixture_data(
     assert row['round'] == '4'
     assert row['date'] == pd.Timestamp('2019-04-23T18:15:38')
     assert row['season'] == '2018/19'
-    assert row['averageGoalsForFixture'] == 6.00
-    assert row['homeTeamID'] == 7901
     assert row['homeTeam'] == 'West Ham United'
-    assert row['homeDaysSinceLastMatch'] == 3
-    assert row['homeAvgScoredLast5'] == 4.33
-    assert row['homeAvgConcededLast5'] == 1.67
-    assert row['homeScoredLastMatch'] == 5
-    assert row['homeConcededLastMatch'] == 2
-    assert row['awayTeamID'] == 496
     assert row['awayTeam'] == 'Tottenham Hotspur'
-    assert row['awayDaysSinceLastMatch'] == 5
-    assert row['awayAvgScoredLast5'] == 1.33
-    assert row['awayAvgConcededLast5'] == 3.67
-    assert row['awayScoredLastMatch'] == 1
-    assert row['awayConcededLastMatch'] == 3
 
 
 @pytest.fixture()
