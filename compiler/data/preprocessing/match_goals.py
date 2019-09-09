80f87ef58e5478d53cf86624b0a46c4bf31e8b63
Joe Sweeny <joesweeny84@hotmail.com>
2019-09-09 12:14:30 +0100

    WIP

--- compiler/data/preprocessing/match_goals.py
+++ compiler/data/preprocessing/match_goals.py
@@ -1,12 +1,18 @@
 from compiler.data.aggregator.match_goals import MatchGoals
 from compiler.data.repository.redis import RedisRepository
 from compiler.framework import config
+from compiler.grpc.proto.fixture.fixture_pb2 import Fixture
+import pandas as pd
+from typing import List
+from compiler.data.calculation import elo, stats
 
 
-class MatchGoalsPreProcessor:
-    __key = "match-goals:competition:{}"
 
-    def __init__(self, aggregator: MatchGoals, cache: RedisRepository, configuration: config):
-        self.__aggregator = aggregator
-        self.__cache = cache
-        self.__configuration = configuration
+def pre_process_historic_data_set(self, results: pd.DataFrame) -> pd.DataFrame:
+# Add team strength columns
+
+# Add ratio columns
+
+# Add average scored columns
+
+# return data frame
