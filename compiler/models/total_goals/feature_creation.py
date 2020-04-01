1f47b9d804d7743f1e9f31d6dde8baf3709ff6ae
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-04-01 15:11:01 +0100

    Add feature creation tests

--- compiler/models/total_goals/feature_creation.py
+++ compiler/models/total_goals/feature_creation.py
@@ -3,13 +3,14 @@ import pandas as pd
 from typing import Dict
 
 
-def calculate_round_stats(fixtures: pd.DataFrame) -> Dict:
+def calculate_round_goal_stats(fixtures: pd.DataFrame) -> Dict:
     """
     Calculate rolling goal scoring stats for each season, round and team within dataframe
     Each round is ordered by the team scoring the most goals across a season
 
     :param fixtures
     :return Dict
+    :raises KeyError
     """
     seasons = fixtures['season'].unique()
     rounds = np.sort(fixtures['round'].unique())
@@ -47,12 +48,17 @@ def calculate_round_stats(fixtures: pd.DataFrame) -> Dict:
 
 
 def __get_previous_round_goals(stats: Dict, team: str, r: int, goals: int):
-    if round == 1:
+    if r == 1:
         return goals
 
-    previous = stats[r-1]
+    one_round_previous = stats[r - 1]
 
-    if team not in previous:
-        raise KeyError("Key " + team + " not found when creating round stats for round " + str(r))
+    if team in one_round_previous:
+        return one_round_previous[team] + goals
 
-    return stats[r-1][team] + goals
+    two_rounds_previous = stats[r - 2]
+
+    if team in two_rounds_previous:
+        return two_rounds_previous[team] + goals
+
+    raise KeyError("Key " + team + " not found when creating round stats for round " + str(r))
