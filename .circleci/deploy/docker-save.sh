741914f66b8ad8964074866191e78c59dd53b54f
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-11-06 15:11:36 +0000

    Update docker save script

--- .circleci/deploy/docker-save.sh
+++ .circleci/deploy/docker-save.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p /tmp/workspace/docker-cache
 
-docker save -o /tmp/workspace/docker-cache/statisticooddscompiler_statisticooddscompilergrpc.tar statisticooddscompiler_grpc:latest
+docker save -o /tmp/workspace/docker-cache/statisticooddscompiler_statistico-oddscompiler-grpc.tar statisticooddscompiler_grpc:latest
